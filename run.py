94b716542459ad5eee1e7f1aa84b7f521285051a
kdr250 <ytkodera4863@gmail.com>
2019-09-14 11:00:30 +0900

    Add-SignUp-and-Login-Logout
    ・データベースをMySQLに設定しました。
    ・サインアップ、ログイン、ログアウト機能を実装しました。

--- run.py
+++ run.py
@@ -1,10 +1,86 @@
-from flask import Flask, render_template
+# Main
+from flask import Flask, render_template, redirect, url_for, flash
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'd50afe8ef1fe6f6934245436e6a52776de99f8fa2b31e766991acaf6ef57'
+
+# Form
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flask_login import LoginManager, current_user, login_user, logout_user
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+class ResigtrationForm(FlaskForm):
+  username = StringField('Username',
+    validators=[DataRequired(), Length(min=2, max=20)])
+  email = StringField('Email',
+    validators=[DataRequired(), Email()])
+  password = PasswordField('Password',
+    validators=[DataRequired()])
+  confirm_password = PasswordField('Confirmation',
+    validators=[DataRequired(), EqualTo('password')])
+  submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+  email = StringField('Email',
+    validators=[DataRequired(), Email()])
+  password = PasswordField('Password', validators=[DataRequired()])
+  submit = SubmitField('Login')
+
+
+# DB
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/flaskblog'
+
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  username = db.Column(db.String(20), unique=True, nullable=False)
+  email = db.Column(db.String(120), unique=True, nullable=False)
+  password = db.Column(db.String(60), nullable=False)
+  posts = db.relationship('Post', backref='author', lazy=True)
+
+  def __repr__(self):
+    return f"User('{self.username}', '{self.email}')"
+
+class Post(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  title = db.Column(db.String(100), nullable=False)
+  date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+  content = db.Column(db.Text, nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+  def __repr__(self):
+    return f"Post('{self.title}', '{self.date_posted}')"
+
 
 @app.route('/')
 def home():
-  post = "Home Page"
-  return render_template('home.html', post=post)
+  posts = [
+    {
+      'author': 'Taro',
+      'title': 'First Post',
+      'contet': 'The first post!',
+      'date_posted': '2019/09/11',
+    },
+    {
+      'author': 'Jiro',
+      'title': 'Secnd Post',
+      'contet': 'The second post!',
+      'date_posted': '2019/09/13',
+    }
+  ]
+  return render_template('home.html', posts=posts)
+  # post = 'hello world'
   # return "Hello World"
 
 @app.route('/about')
@@ -13,6 +89,41 @@ def about():
   title = "About"
   return render_template('about.html', post=post, title=title)
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  if current_user.is_authenticated:
+    return redirect(url_for('home'))
+  form = ResigtrationForm()
+  if form.validate_on_submit():
+    # password = form.password.data
+    hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+    user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+    db.session.add(user)
+    db.session.commit()
+    flash(f'Account Created! Now Login!', 'success')
+    return redirect(url_for('home'))
+  return render_template('register.html', form=form, title='Register')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  if current_user.is_authenticated:
+    return redirect(url_for('home'))
+  form = LoginForm()
+  if form.validate_on_submit():
+    user = User.query.filter_by(email=form.email.data).first()
+    if user and bcrypt.check_password_hash(user.password, form.password.data):
+      # login_user(user)
+      login_user(user)
+      flash(f'You Login!', 'success')
+      return redirect(url_for('home'))
+  return render_template('login.html', form=form, title='Login')
+
+@app.route('/logout')
+def logout():
+  logout_user()
+  return redirect(url_for('home'))
+
+
 if __name__ == '__main__':
-  app.run()
+  app.run(debug=True)
 
