fb18ac652288606030872e8bd14d72812ffe29e5
kdr250 <ytkodera4863@gmail.com>
2019-10-24 01:45:22 +0900

    ビューを追加修正
    ・トップページ(index)を追加しました
    ・チャット画面を改善しました。

--- static/js/post.js
+++ static/js/post.js
@@ -1,19 +1,43 @@
 $(function() {
 
   function buildHTML(post){
-    var html = 0
-    if (post.same == 1) {
-      var html_same = `<div class="post-content card col-sm-11 col-md-11 bg-info" data-post-id=${post.id}>`
-    } else { 
-      var html_same = `<div class="post-content card col-sm-11 col-md-11" data-post-id=${post.id}>`
-    }
-    var html = `<div class="card-body">
-      <h5 class="card-title">${post.title}</h5>
-      <h6 class="card-subtitle mb-2">投稿日: ${post.date_posted} By ${post.authorname}</h6>
-      <p class="card-text">${post.content}</p>
-      </div>
+    // var html = ''
+    // if (post.same == 1) {
+    //   var html_same = `<div class="post-content card col-sm-11 col-md-11 bg-info" data-post-id=${post.id}>`
+    // } else { 
+    //   var html_same = `<div class="post-content card col-sm-11 col-md-11" data-post-id=${post.id}>`
+    // }
+    // var html = `<div class="card-body">
+    //   <h5 class="card-title">${post.title}</h5>
+    //   <h6 class="card-subtitle mb-2">投稿日: ${post.date_posted} By ${post.authorname}</h6>
+    //   <p class="card-text">${post.content}</p>
+    //   </div>
+    //   </div>`
+    // html = html_same + html
+    if (post.same == 1){
+      var html = `<div class="media post-right" data-post-id=${post.id}>
+        <div class="balloon1-right">
+          <h5>${post.title}  <small>by ${post.authorname}</small></h5>
+          <p>${post.content}</p>
+        </div>
       </div>`
-    html = html_same + html
+    } else if (post.authorname == 'HarryBotter') {
+      var html = `<div class="media" data-post-id=${post.id}>
+        <img src="static/images/chatbot_icon.png" class="mr-3 post-pict" alt="...">
+        <div class="balloon1-left">
+            <h5>${post.title}  <small>by ${post.authorname}</small></h5>
+            <p>${post.content}</p>
+        </div>
+      </div>`
+    } else {
+      var html = `<div class="media" data-post-id=${post.id}>
+        <img src="static/images/user_icon.png" class="mr-3 post-pict" alt="...">
+        <div class="balloon1-left">
+          <h5>${post.title}  <small>by ${post.authorname}</small></h5>
+          <p>${post.content}</p>
+        </div>
+      </div>`
+    }
     return html
   }
 
@@ -39,7 +63,7 @@ $(function() {
   var reloadMessages = function() {
     // lastメソッドはidではなくクラス指定(class="~" => $('.~:last'))出ないと効かない!
     // last_post_id = $('#post-content').last().data('post-id')
-    var last_post_id = $('.post-content:last').data('post-id')
+    var last_post_id = $('.media:last').data('post-id')
     console.log(last_post_id)
     $.ajax({
       //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
