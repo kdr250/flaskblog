e168a2ce5156ffe92af5d28f89d5d59cf0ecb56c
kdr250 <ytkodera4863@gmail.com>
2019-10-26 21:44:03 +0900

    DBパスの記述位置を修正
    DBパスの記述位置を修正

--- config.py
+++ config.py
@@ -12,8 +12,9 @@ app.config['SECRET_KEY'] = 'd50afe8ef1fe6f6934245436e6a52776de99f8fa2b31e766991a
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 
-db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'mysql://root:@localhost/flaskblog'
+db = SQLAlchemy(app)
+
 
 # SQLiteの場合は下記
 # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
